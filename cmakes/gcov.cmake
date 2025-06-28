f94da8ed9e0b2aac207ee79fbd6659f362865c73
Keidan (K. Billonneau) <3134348+Keidan@users.noreply.github.com>
2025-06-28 02:30:00 +0200

    gcov config

--- cmakes/gcov.cmake
+++ cmakes/gcov.cmake
@@ -1,33 +1,16 @@
 
 if(DEFINED IS_DEBUG AND IS_DEBUG EQUAL 1 AND NOT DEFINED WIN32)
+  set(OBJECT_DIR ${CMAKE_BINARY_DIR}/CMakeFiles/${CMAKE_PROJECT_NAME}.dir)
   # Create the gcov target. Run coverage tests with 'make gcov'
   add_custom_target(gcov
     COMMAND mkdir -p coverage
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
 
-  set(OBJECT_DIR ${CMAKE_BINARY_DIR}/CMakeFiles/${CMAKE_PROJECT_NAME}.dir)
   add_custom_command(TARGET gcov
-    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/ntpc/NTPClient.cpp -o ${OBJECT_DIR}/src/ntpc/
-    | grep -A 5 "NTPClient.cpp" > CoverageSummary.tmp
-    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/ntpc/UDPSocket.cpp -o ${OBJECT_DIR}/src/ntpc/
-    | grep -A 5 "UDPSocket.cpp" >> CoverageSummary.tmp
-    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/main.cpp -o ${OBJECT_DIR}/src/
-    | grep -A 5 "main.cpp" >> CoverageSummary.tmp
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coverage
-  )
-
-  add_custom_command(TARGET gcov
-    COMMAND echo "=================== GCOV ===================="
-    COMMAND cat CoverageSummary.tmp
-    COMMAND echo "-- Coverage files have been output to ${CMAKE_BINARY_DIR}/coverage"
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coverage
-  )
-
-  add_custom_command(TARGET gcov POST_BUILD
-    COMMAND lcov --directory ${OBJECT_DIR} -c -o ${CMAKE_BINARY_DIR}/coverage/rapport.info
-    COMMAND lcov --remove ${CMAKE_BINARY_DIR}/coverage/rapport.info -o ${CMAKE_BINARY_DIR}/coverage/rapport_filtered.info
-    COMMAND genhtml -o . -t "Test cover" rapport_filtered.info
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/ntpc/NTPClient.cpp.gcno -o ${OBJECT_DIR}/src/ntpc/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/ntpc/UDPSocket.cpp.gcno -o ${OBJECT_DIR}/src/ntpc/
+    COMMAND gcov -b ${CMAKE_SOURCE_DIR}/src/main.cpp.gcno -o ${OBJECT_DIR}/src/
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coverage
   )
   
